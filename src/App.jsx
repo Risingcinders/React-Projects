209f05af79157c8710ab1e8042cc7ad49cbd5803
Seantay <risingcinders@gmail.com>
2020-10-29 16:29:13 -0500

    default react site

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Boxdiv from './components/boxdiv'
+
+function App() {
+  return (
+    <div className="App">
+      <Boxdiv />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/boxdiv.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import './styles.modules.css'
+
+
+const Boxdiv = (props) => {
+    return(
+        <div>What's up</div>
+    )
+
+};
+
+export default Boxdiv;
