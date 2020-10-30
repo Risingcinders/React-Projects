3630f83f2c2c443acaed7dee42e8014372d48664
Seantay <risingcinders@gmail.com>
2020-10-30 12:05:28 -0500

    ninja bonus

--- src/components/picker.jsx
+++ src/components/picker.jsx
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./styles.modules.css";
 
 const Picker = (props) => {
     const [color, setColor] = useState("peachpuff");
+    const textInput = useRef(null)
 
     const handleSubmit = (e) => {
         e.preventDefault();
         props.whenSubmitted( color )
+        textInput.current.value = ""
+        
     };
 
     return (
         <div className="picker">
-            <form onSubmit={ handleSubmit }>
+            <form onSubmit={ handleSubmit } >
                 <label htmlFor="">
                     New Box Color:   
-                    <input type="text" onChange={ (e) => setColor(e.target.value) } />
+                    <input ref={textInput} type="text" onChange={ (e) => setColor(e.target.value) } />
                 </label>
                 <button type="submit">
                     Create Box
