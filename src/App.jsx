fb3f53eead6cbccb4fa32d73ecdafeba57b7c6b5
Seantay <risingcinders@gmail.com>
2020-10-30 16:32:52 -0500

    Tabs Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
 
 import Tab from "./components/tab";
 
 function App() {
+
     return (
         <div className="App">
             <Tab/>
--- src/components/tab.jsx
+++ src/components/tab.jsx
@@ -2,33 +2,35 @@ import React, { useState } from "react";
 import "./styles.modules.css";
 
 const Tab = (props) => {
-    const [selectedContent, setSelectedContent] = useState("Click a tab to update")
+    const [selectedContent, setSelectedContent] = useState(
+        "Click a tab to update"
+    );
 
     const initialState = [
         ["Tab 1", "Tab 1 Content is a bunch of words"],
         ["Tab 2", "Tab 2 Content is totally different words"],
         [
             "Tab 3",
-            "Tab 3 Content is basically the same words in a different order"
-        ]
-    ]
+            "Tab 3 Content is basically the same words in a different order",
+        ],
+    ];
 
-    const [varObj, setVarObj] = useState();
-
-    const refreshContent = (b) => {
-        setSelectedContent(varObj[b])
+    const [varObj, setVarObj] = useState(initialState);
+    const handler = (e, value) => {
+        setSelectedContent(value.peanut)
     }
-
-    const genTabs = (arr) => {
-        for (var i = 0; i < 3; i++) {
-            <h5 className="tabby" onClick={refreshContent(i)} > {arr[i]} </h5>;
-        }
-    };
 
     return (
         <div>
-            {/* <div> { genTabs(varObj) } </div> */}
-            <div id="contents"> { selectedContent }  </div>
+            <div id="tablist"> 
+            { varObj.map( (item, index) => {
+                return <h5
+                onClick={ (e) => { handler(e, {peanut : varObj[index][1]}) } }
+                className="tabby">
+                {varObj[index][0]}
+            </h5>
+            } )}</div>
+            <div id="contents"> {selectedContent} </div>
         </div>
     );
 };
